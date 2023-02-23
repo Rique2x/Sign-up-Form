ebbe126cfaa39f3b9138f165cf3e4d569da6a868
Rique2 <enrique2heinrich@gmail.com>
2023-02-23 18:15:00 +0200

    updating web page 1 (css)(js)

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 function confirmPass() {
-    
-    const pass = document.getElementById("password").value
-    const confPass = document.getElementById("confirm-password").value
-    
-    if(pass != confPass) {
-        //alert('Password doesnt match!');
-        document.getElementById('error').innerHTML='Password doesnt match!';
-    }
-    else
-    {
-        document.getElementById('error').innerHTML='';
-    }
+  const pass = document.getElementById("password").value;
+  const confPass = document.getElementById("confirm-password").value;
+
+  if (pass != confPass) {
+    //alert('Password doesnt match!');
+    document.getElementById("error").innerHTML = "Password doesnt match!";
+  } else {
+    document.getElementById("error").innerHTML = "";
+  }
 }
